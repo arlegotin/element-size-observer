bb32db16ab29baf82bc4e51bf7d43d3fe12c62c3
Artem Legotin <arlegotin@gmail.com>
2022-10-25 18:06:15 +0200

    Just do it

---
+++ dist/index.cjs.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var EventEmitter = require('events');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var EventEmitter__default = /*#__PURE__*/_interopDefaultLegacy(EventEmitter);
+
+class ElementSizeObserver extends EventEmitter__default["default"] {
+  /**
+   * @typedef {String} SizeObserverEventName
+   */
+  static EVENT_SIZE_CHANGED = 'size-changed';
+
+  /**
+   * @param {Object} params
+   * @param {Element} params.element – observable DOM-element
+   * @param {Function|null}
+   */
+  constructor({
+    element,
+    onResize = null
+  }) {
+    super();
+    this.element = element;
+    this.observer = new ResizeObserver(this.onObserverChanged.bind(this));
+    if (onResize) {
+      this.on(ElementSizeObserver.EVENT_SIZE_CHANGED, onResize);
+    }
+    this.observer.observe(this.element);
+  }
+
+  /**
+   * Destroys the instance
+   * @returns {void}
+   */
+  destructor() {
+    this.removeAllListeners();
+    this.observer.unobserve(this.element);
+    this.observer.disconnect();
+  }
+
+  /**
+   * Processes list of entries
+   * @param {Array.<ResizeObserverEntry>} entries 
+   * @returns {void}
+   */
+  onObserverChanged(entries) {
+    const entry = entries.find(e => e.target === this.element);
+    if (entry) {
+      this.emit(ElementSizeObserver.EVENT_SIZE_CHANGED, {
+        element: this.element
+      });
+    }
+  }
+}
+
+module.exports = ElementSizeObserver;
---
+++ src/index.js
@@ -0,0 +1,53 @@
+import EventEmitter from 'events'
+
+export default class ElementSizeObserver extends EventEmitter {
+
+  /**
+   * @typedef {String} SizeObserverEventName
+   */
+  static EVENT_SIZE_CHANGED = 'size-changed'
+
+  /**
+   * @param {Object} params
+   * @param {Element} params.element – observable DOM-element
+   * @param {Function|null}
+   */
+  constructor({ element, onResize = null }) {
+    super()
+
+    this.element = element
+
+    this.observer = new ResizeObserver(this.onObserverChanged.bind(this))
+
+    if (onResize) {
+      this.on(ElementSizeObserver.EVENT_SIZE_CHANGED, onResize)
+    }
+    
+    this.observer.observe(this.element)
+  }
+
+  /**
+   * Destroys the instance
+   * @returns {void}
+   */
+  destructor() {
+    this.removeAllListeners()
+    this.observer.unobserve(this.element)
+    this.observer.disconnect()
+  }
+
+  /**
+   * Processes list of entries
+   * @param {Array.<ResizeObserverEntry>} entries 
+   * @returns {void}
+   */
+  onObserverChanged(entries) {
+    const entry = entries.find(e => e.target === this.element)
+
+    if (entry) {
+      this.emit(ElementSizeObserver.EVENT_SIZE_CHANGED, {
+        element: this.element,
+      })
+    }
+  }
+}
